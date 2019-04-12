9df2a073e93041fdaf100af3c7e073cb26b180eb
Antoine <antoine.sb@orange.fr>
2019-04-12 16:04:51 +0100

    :ambulance: bugfixes :recycle: refactor code

--- application/database.php
+++ application/database.php
@@ -1,20 +1,17 @@
 <?php
 
-$mysqli->set_charset("utf8"); // really necessary ?
-
-$database_user = array(
+/* GLOBALS */
+$GLOBALS["database_read"] = new stdClass();
+$GLOBALS["database_write"] = new stdClass();
+$GLOBALS["database_user"] = array(
 	"ewsc_r" => "t9x.g}sTL|EF",
 	"ewsc_w" => "\IiT42_:@VUW",
 );
 
-/* HANDLES */
-$database_read = NULL;
-$database_write = NULL;
-
 function get_database_connection(object $database, string $username) : object {
-	if(is_null($database)) {
+	if($database instanceof stdClass) {
 		try {
-			$database = new PDO('mysql:host=localhost;dbname=ewsc;charset=utf8', $username, $database_user[$username], array(PDO::ATTR_PERSISTENT => true));
+			$database = new PDO('mysql:host=localhost;dbname=ewsc;charset=utf8', $username, $GLOBALS["database_user"][$username], array(PDO::ATTR_PERSISTENT => true));
 			$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 		catch(PDOException $e) {
@@ -26,100 +23,111 @@ function get_database_connection(object $database, string $username) : object {
 }
 
 /* READ */
-function get_user_feed(string $email, date $since = time()) : array {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+function get_user_feed(string $email, date $since = null) : array {
+	if(is_null($since))
+		$since = time();
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
+
+	$feed = array();
 
 	$statement = $database_handle->prepare(
-		'SELECT feed, release_date, title, description, content
-		FROM publications
-		WHERE release_date > ? AND feed IN (
-			SELECT feed FROM subscriptions WHERE user_email = ?
+		'SELECT feed_url, published, title, summary, source
+		FROM publications_atom
+		WHERE published > ? AND feed IN (
+			SELECT feed_url FROM subscriptions WHERE user_email = ?
 		)
-		ORDER BY release_date'
+		ORDER BY published'
 	);
 	$statement->bindValue(1, $since, PDO::PARAM_STR);
-	$statement->bindValue(2, mysqli_real_escape_string($email), PDO::PARAM_STR);
+	$statement->bindValue(2, $email, PDO::PARAM_STR);
 
-	return perform_return_transaction($database_handle, $statement);
+	$feed[0] = perform_return_transaction($database_handle, $statement);
+
+	$statement = $database_handle->prepare(
+		'SELECT feed_url, pubDate, title, description, textInput
+		FROM publications_rss
+		WHERE pubDate > ? AND feed IN (
+			SELECT feed_url FROM subscriptions WHERE user_email = ?
+		)
+		ORDER BY pubDate'
+	);
+	$statement->bindValue(1, $since, PDO::PARAM_STR);
+	$statement->bindValue(2, $email, PDO::PARAM_STR);
+
+	$feed[1] = perform_return_transaction($database_handle, $statement);
+
+	return $feed;
 }
 
 function get_user_subscriptions(string $email) : array {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
 
 	$statement = $database_handle->prepare('SELECT feed_url FROM subscriptions WHERE user_email = ?');
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
 
 	return perform_return_transaction($database_handle, $statement);
 }
 
-function user_exists(string $email) : bool {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+function email_exists(string $email) : bool {
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
 
-	$statement = $database_handle->prepare(
-		'SELECT CASE WHEN EXISTS (
-			SELECT *
-			FROM users
-			WHERE email = ?
-		)
-		THEN CAST(1 AS BIT)
-		ELSE CAST(0 AS BIT) END'
-	);
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
+	$statement = $database_handle->prepare('SELECT email FROM users WHERE email = ?');
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
 
-	return perform_return_transaction($database_handle, $statement);
+	return sizeof(perform_return_transaction($database_handle, $statement)) == 1;
 }
 
 function get_hash(string $email) : string {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
 
 	$statement = $database_handle->prepare('SELECT hash FROM users WHERE email = ?');
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
 
-	return perform_return_transaction($database_handle, $statement);
+	return perform_return_transaction($database_handle, $statement)[0][0];
 }
 
 function is_subscribed(string $email, string $url) : bool {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
 
 	$statement = $database_handle->prepare('SELECT * FROM publications WHERE user_email = ? AND feed_url = ?');
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
-	$statement->bindValue(2, mysqli_real_escape_string($url), PDO::PARAM_STR);
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
+	$statement->bindValue(2, $url, PDO::PARAM_STR);
 
-	return perform_return_transaction($database_handle, $statement);
+	return sizeof(perform_return_transaction($database_handle, $statement)) == 1;
 }
 
 function get_feeds() : array {
-	$database_handle = get_database_connection($database_read, "ewsc_r");
+	$database_handle = get_database_connection($GLOBALS["database_read"], "ewsc_r");
 	return perform_return_transaction($database_handle, $database_handle->prepare('SELECT * FROM feeds'));
 }
 
 /* WRITE */
-function create_account(string $email, string $hash, array $urls) : bool {
-	$database_handle = get_database_connection($database_write, "ewsc_w");
+function create_account(string $email, string $hash) : bool {
+	$database_handle = get_database_connection($GLOBALS["database_write"], "ewsc_w");
 
 	$statement = $database_handle->prepare('INSERT IGNORE INTO users (email, hash, last_connection) VALUES (?, ?, CURRENT_TIMESTAMP)');
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
-	$statement->bindValue(2, mysqli_real_escape_string($hash), PDO::PARAM_STR);
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
+	$statement->bindValue(2, $hash, PDO::PARAM_STR);
 
 	return perform_transaction($database_handle, array($statement));
 }
 
 function add_feed(string $email, string $feed) : bool {
-	$database_handle = get_database_connection($database_write, "ewsc_w");
+	$database_handle = get_database_connection($GLOBALS["database_write"], "ewsc_w");
 
 	$statement = $database_handle->prepare('INSERT IGNORE INTO subscriptions (user_email, feed_url) VALUES (?, ?)');
-	$statement->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
-	$statement->bindValue(2, mysqli_real_escape_string($feed), PDO::PARAM_STR);
+	$statement->bindValue(1, $email, PDO::PARAM_STR);
+	$statement->bindValue(2, $feed, PDO::PARAM_STR);
 
 	return perform_transaction($database_handle, array($statement));
 }
 
 function remove_feed(string $email, string $feed) {
-	$database_handle = get_database_connection($database_write, "ewsc_w");
+	$database_handle = get_database_connection($GLOBALS["database_write"], "ewsc_w");
 
 	$statement_0 = $database_handle->prepare('DELETE subscriptions WHERE user_email = ? AND feed_url = ?');
-	$statement_0->bindValue(1, mysqli_real_escape_string($email), PDO::PARAM_STR);
-	$statement_0->bindValue(2, mysqli_real_escape_string($feed), PDO::PARAM_STR);
+	$statement_0->bindValue(1, $email, PDO::PARAM_STR);
+	$statement_0->bindValue(2, $feed, PDO::PARAM_STR);
 
 	perform_transaction($database_handle, array($statement_0));
 }
@@ -166,6 +174,6 @@ function perform_return_transaction(object $database, object $statement) : array
 		$database->rollBack();
 		echo "Failed: " . $e->getMessage();
 
-		return null;
+		return array();
 	}
 }
--- application/sign.php
+++ application/sign.php
@@ -2,50 +2,58 @@
 
 /* VALIDATION */
 function validate_email(string $email) : bool {
-	return preg_match("\A[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z", $email) === 1
-		&& strlen($email) <= 64;
+	return filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($email) <= 64;
 }
 
 function validate_password(string $password) : bool {
-	return preg_match("[[:graph:]]{8,32}", $password) === 1
-		&& preg_match("[[:lower:]]+", $password) === 1
-		&& preg_match("[[:upper:]]+", $password) === 1
-		&& preg_match("\d+", $password) === 1
-		&& strlen($password) <= 32;
+	return 7 < strlen($password) && strlen($password) < 33
+		&& preg_match("/[a-z]/", $password)
+		&& preg_match("/[A-Z]/", $password)
+		&& preg_match("/[0-9]/", $password);
 }
 
 /* QUICKCHECKS */
 function is_remember() : bool {
-	return isset($_POST["remember"]) && $_POST["remember"] === true;
+	return isset($_POST["remember"]) && $_POST["remember"] == true;
 }
 
-function is_signed_in() {
+function is_signed_in() : bool {
 	return isset($_SESSION["email"]) && isset($_SESSION["password"]);
+}
+
+function post_credentials_exists() : bool {
+	return isset($_POST["email"]) && isset($_POST["password"]);
+}
+
+/* SECONDARY */
+function start_session() {
+	session_name($_POST["email"]);
+	is_remember() ? session_start() : session_start(['cookie_lifetime' => 86400]);
+	$_SESSION["email"] = $_POST["email"];
+	$_SESSION["password"] = $_POST["password"];
+}
+
+function attempt_session_with_password() : bool {
+	if(password_verify($_POST["password"], get_hash($_POST["email"]))) {
+		start_session();
+		return true;
+	}
+	return false;
+}
+
+function get_username() : string {
+	return explode('@', $_SESSION["email"])[0];
 }
 
 /* PRIMARY */
 function sign() : int {
-	if(is_signed_in()) {
-		return 200;
-	} elseif(isset($_POST["email"]) && isset($_POST["password"])) {
-		if(validate_email($_POST["email"]) && validate_password($_POST["password"])) {
-			if(user_exists($_POST["email"])) {
-				if(password_verify($_POST["password"], get_hash($_POST["email"]))) {
-					remember();
-					return 200;
-				}
-				return 401;
-			}
-			create_account($_POST["email"], password_hash($_POST["password"], PASSWORD_DEFAULT));
-			return 201;
+	if(post_credentials_exists() && validate_email($_POST["email"]) && validate_password($_POST["password"])) {
+		if(email_exists($_POST["email"])) {
+			return attempt_session_with_password() ? 200 : 401;
 		}
+		create_account($_POST["email"], password_hash($_POST["password"], PASSWORD_DEFAULT));
+		start_session();
+		return 201;
 	}
 	return 400;
-}
-
-function remember() {
-	if(is_remember()) {
-		$_SESSION["email"] = $_POST["email"];
-		$_SESSION["password"] = $_POST["password"];
-	}
 }
